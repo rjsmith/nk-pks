a5baa7a1987bb50a53b0a76b0f9cf878dceed4fd
Richard Smith <adadcalledrichard@gmail.com>
2014-05-13 17:58:36 +0100

    Completed PKS diary part 7

--- urn.uk.co.rsbatechnology.pks/uk/co/rsbatechnology/pks/sinkPositionDelta.groovy
+++ urn.uk.co.rsbatechnology.pks/uk/co/rsbatechnology/pks/sinkPositionDelta.groovy
@@ -32,14 +32,13 @@
 
 import org.netkernel.layer0.representation.*
 import org.netkernel.layer0.representation.impl.*;
+import org.netkernel.layer0.nkf.*;
 
 POSITION_IDENTIFIER_DELIMITER = ":"
+POSITION_DELTA_PDS_PREFIX = "pds:/pks/delta/"
 
-println "businessArea value: "+ context.getThisRequest().getArgumentValue("businessArea")
+// Parse common request arguments
 businessArea = context.source("arg:businessArea")
-
-println "businessArea:" +businessArea
-
 location = context.source("arg:location")
 lifecycleType = context.source("arg:lifecycleType")
 accountUUID = context.source("arg:accountUUID")
@@ -48,39 +47,53 @@ positionDate = context.source("arg:positionDate")
 positionSymbol = context.source("arg:positionSymbol")
 deltaSequence = context.source("arg:deltaSequence")
 
-//location = context.getThisRequest().getArgumentValue("location")
-//lifecycleType = context.getThisRequest().getArgumentValue("lifecycleType")
-//accountUUID = context.getThisRequest().getArgumentValue("accountUUID")
-//positionType = context.getThisRequest().getArgumentValue("positionType")
-//positionDate = context.getThisRequest().getArgumentValue("positionDate")
-//positionSymbol = context.getThisRequest().getArgumentValue("positionSymbol")
-//deltaSequence = context.getThisRequest().getArgumentValue("deltaSequence")
-
-IHDSNode sourceTransaction = context.source("arg:sourceTransaction", IHDSNode.class)
-IHDSNode firstSourceTransactionNode = sourceTransaction.getFirstNode("/sourceTransaction")
-// TODO: Check if null, throw exception
-
-IHDSNode deltaAmounts = context.sourcePrimary(IHDSNode.class)
-IHDSNode firstAmountsNode = deltaAmounts.getFirstNode("/deltaAmounts")
-// TODO: Check if null, throw exception
-
 positionDeltaIdentifier = businessArea+
-	POSITION_IDENTIFIER_DELIMITER+location+
-	POSITION_IDENTIFIER_DELIMITER+lifecycleType+
-	POSITION_IDENTIFIER_DELIMITER+accountUUID+
-	POSITION_IDENTIFIER_DELIMITER+positionType+
-	POSITION_IDENTIFIER_DELIMITER+positionDate+
-	POSITION_IDENTIFIER_DELIMITER+positionSymbol+
-	POSITION_IDENTIFIER_DELIMITER+deltaSequence
+POSITION_IDENTIFIER_DELIMITER+location+
+POSITION_IDENTIFIER_DELIMITER+lifecycleType+
+POSITION_IDENTIFIER_DELIMITER+accountUUID+
+POSITION_IDENTIFIER_DELIMITER+positionType+
+POSITION_IDENTIFIER_DELIMITER+positionDate+
+POSITION_IDENTIFIER_DELIMITER+positionSymbol+
+POSITION_IDENTIFIER_DELIMITER+deltaSequence
 
+switch (context.getThisRequest().getVerb())
+{
+	case INKFRequestReadOnly.VERB_SINK:
+		IHDSNode sourceTransaction = context.source("arg:sourceTransaction", IHDSNode.class)
+		IHDSNode firstSourceTransactionNode = sourceTransaction.getFirstNode("/sourceTransaction")
+		// TODO: Check if null, throw exception
+		
+		IHDSNode deltaAmounts = context.sourcePrimary(IHDSNode.class)
+		IHDSNode firstAmountsNode = deltaAmounts.getFirstNode("/deltaAmounts")
+		// TODO: Check if null, throw exception
+		
+		// Create HDS structure
+		b = new HDSBuilder()
+		b.pushNode("positionDelta")
+		b.addNode("@id", positionDeltaIdentifier)
+		b.addNode("deltaSequence", deltaSequence)
+		b.importNode(firstAmountsNode)
+		b.importNode(firstSourceTransactionNode)
+		
+		// Use PDS to SINK position delta node
+		context.sink(POSITION_DELTA_PDS_PREFIX+positionDeltaIdentifier, b.getRoot())
+		break
+	case INKFRequestReadOnly.VERB_SOURCE:
+		request = context.createRequest(POSITION_DELTA_PDS_PREFIX+positionDeltaIdentifier)
+		response = context.issueRequestForResponse(request)
+		context.createResponseFrom(response)
+		break
+	case INKFRequestReadOnly.VERB_DELETE:
+		request = context.createRequest(POSITION_DELTA_PDS_PREFIX+positionDeltaIdentifier)
+		request.setVerb(INKFRequestReadOnly.VERB_DELETE)
+		response = context.issueRequestForResponse(request)
+		context.createResponseFrom(response)
+		break
+	case INKFRequestReadOnly.VERB_EXISTS:
+		request = context.createRequest(POSITION_DELTA_PDS_PREFIX+positionDeltaIdentifier)
+		request.setVerb(INKFRequestReadOnly.VERB_EXISTS)
+		response = context.issueRequestForResponse(request)
+		context.createResponseFrom(response)
+		break
+}
 
-// Create HDS structure
-b = new HDSBuilder()
-b.pushNode("positionDelta")
-b.addNode("@id", positionDeltaIdentifier)
-b.addNode("deltaSequence", deltaSequence)
-b.importNode(firstAmountsNode)
-b.importNode(firstSourceTransactionNode)
-
-// Use PDS to SINK position delta node
-context.sink("pds:/pks/delta/"+positionDeltaIdentifier, b.getRoot())
